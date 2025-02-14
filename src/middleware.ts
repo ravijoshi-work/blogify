d0d3a77651fc4f7cf54062ca72f101a97b4d5078
ravijoshi-wki <ravijoshi@webkreators.in>
2025-02-15 00:03:36 +0530

    fixed build errors

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,32 +1,32 @@
-import { NextRequest, NextResponse } from "next/server";
-import { authMiddleware } from "./middleware/auth";
-import {
-  apiPrefix,
-  apiPublicRoutes,
-  clientPublicRoutes,
-} from "./utils/constant";
+// import { NextRequest, NextResponse } from "next/server";
+// import { authMiddleware } from "./middleware/auth";
+// import {
+//   apiPrefix,
+//   apiPublicRoutes,
+//   clientPublicRoutes,
+// } from "./utils/constant";
 
-export async function middleware(req: NextRequest) {
-  const path = req.nextUrl.pathname; // Get the current URL path
+// export async function middleware(req: NextRequest) {
+//   const path = req.nextUrl.pathname; // Get the current URL path
 
-  const isApiRoute = path.startsWith(apiPrefix);
+//   const isApiRoute = path.startsWith(apiPrefix);
 
-  if (isApiRoute) {
-    if (apiPublicRoutes.includes(path)) {
-      console.log("ready to go", path);
-    }
-  }
+//   if (isApiRoute) {
+//     if (apiPublicRoutes.includes(path)) {
+//       console.log("ready to go", path);
+//     }
+//   }
 
-  // console.log("path", path);
-  // if (clientPublicRoutes.includes(path)) {
-  //   return NextResponse.next(); // Proceed with the request
-  // }
-  // return authMiddleware(req);
+//   // console.log("path", path);
+//   // if (clientPublicRoutes.includes(path)) {
+//   //   return NextResponse.next(); // Proceed with the request
+//   // }
+//   // return authMiddleware(req);
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-// don't run middleware on these routes
-export const config = {
-  matcher: ["/api/:path*"],
-};
+// // don't run middleware on these routes
+// export const config = {
+//   matcher: ["/api/:path*"],
+// };
